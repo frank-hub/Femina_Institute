99f6bb4fff45befa40011c069f6f49f7051f965d
frank-hub <franklinekevo@gmail.com>
2018-10-30 10:51:50 +0300

    initial

---
+++ courses/nav.php
@@ -0,0 +1,47 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-info">
+  <a class="navbar-brand  text-white" href="#">
+  <h3 style="font-weight:bold; class="text-center"><span style="color: #FFCB99">FEMINA</span> TRAINING <br>INSTITUTE</span></h3></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav mx-auto">
+      <li class="nav-item active">
+        <a class="nav-link text-white" href="../">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white" style="" href="../about.html">About Us</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Courses
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="packages.php">Computer Packages</a>
+          <a class="dropdown-item" href="repair.php">Computer Repair & Maintenance</a>
+          <a class="dropdown-item" href="account.php">Accounting(CPA)</a>
+          <a class="dropdown-item" href="business.php">Business Management</a>
+          <a class="dropdown-item" href="youth.php">Youth Mentorship</a>
+          <a class="dropdown-item" href="electric.php">Basic Electrical And Electronics</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white" style="" href="../fee.html">Fees Payment</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link  text-white" style="" href="../downloads.html">Downloads</a>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link text-white" style="" href="../contact.html">Contact Us</a>
+      </li>
+    </ul>
+    <div class="ml-auto"><div class="ml-auto">
+    <p><a href="www.facebook.com/feminainstitute" style="color: #030099" ><i class="fab fa-facebook-f" style="margin-right: 10px"></i></a>
+    <a href="www.twitter.com/feminainstitute" style="color: #030099"><i class="fab fa-twitter" style="margin-right: 10px"></i></a>
+    <a href="www.whatsapp.com/0722953886" style="color: #009834"><i class="fab fa-whatsapp"></i></a></p>
+        </div>
+
+        </div>
+  </div>
+</nav>
